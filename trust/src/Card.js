990a013ee28ceac2761a4d3785068320dc59107f
sriram21088 <sriram21088@gmail.com>
2020-04-17 16:25:32 +0530

    secondary portal added.

---
+++ trust/src/Card.js
@@ -0,0 +1,26 @@
+import React from  'react';
+
+
+
+export default class Card extends  React.Component{
+   constructor(props){
+       super(props);
+       this.elRef = React.createRef();
+   }
+    componentDidMount(){
+        console.log(this.props.keys)
+        setTimeout(() => {
+            let el = this.elRef.current;
+            el.className = "card opacity1";
+        }, (this.props.__i+1) *150);
+       
+    }
+    render(){
+        return (
+              <div ref = {this.elRef} className = "card">
+                  <b className = "cardTitle" >{this.props.Name}</b>
+                <p className = "cardValue" >{this.props.Value}</p>
+              </div>
+        )
+    }
+}
