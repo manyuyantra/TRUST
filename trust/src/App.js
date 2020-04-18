0616c215dfaa397dbba9a777ddd291c450c06a90
sriram21088 <sriram21088@gmail.com>
2020-04-18 16:47:20 +0530

    Secondary portal elements.

--- trust/src/App.js
+++ trust/src/App.js
@@ -5,22 +5,24 @@ import protectedroute from './protectedroute';
 import auth from './auth';
 import Portal from './Portal'
 import LoginForm from './LoginForm';
-import Banner from './Banner'
+import Banner from './Banner';
+import Members from './Members';
+import Reciept from './Reciept';
+import Transactions from './Transactions';
+import Account from './Account'; 
 
 function App() {
       return (
             <Router>
                   <Banner></Banner>
                   <Switch>
-                        <Route path="/" exact render={(props) => {
-                              return (
-                                    <LoginForm {...props}></LoginForm>
-
-                              )
-                        }}></Route>
-                        <Route path="/main" render={() => {
-                              return <Portal />
-                        }}></Route>
+                        <Route path="/" exact  component={LoginForm}></Route>
+                        <Route path="/main" exact  component={Portal}></Route>
+                        <Route  path ="/members" component={Members}></Route>
+                        <Route  path ="/account" component={Account}></Route>
+                        <Route  path ="/transactions" component={Transactions}></Route>
+                        <Route  path ="/reciept" component={Reciept}></Route>
+                        <Route path ="*" render={()=>"No DATA.."}></Route>
                   </Switch>
 
             </Router>
---
+++ trust/src/Members.js
@@ -0,0 +1,6 @@
+import react from 'react';
+
+
+export default function Members(){
+    return "members";
+}
