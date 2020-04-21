01f6763af81264638c5c8c25712d6576bd320a7a
sriram21088 <sriram21088@gmail.com>
2020-04-21 18:11:42 +0530

    memebers table address added.

--- trust/src/Members.js
+++ trust/src/Members.js
@@ -1,43 +1,50 @@
-import React from 'react'
-
+import React from "react";
 
 export default class Members extends React.Component {
-    constructor(pr) {
-        super(pr);
-        let arr = [];
+  constructor(pr) {
+    super(pr);
+    let arr = [];
 
-        for(let  i =0;i<50;i++){
-            arr.push({ "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" })
-        }
-        this.state = {
-            "members": arr
-
-        }
-        this.generateRow = this.generateRow.bind(this);
+    for (let i = 0; i < 50; i++) {
+      arr.push({
+        Name: "Dummy",
+        Email: "DDUMMY@MAILME.COM",
+        CONTACT: "#922311",
+        Address: "#123, street number,city, state"
+      });
     }
+    this.state = {
+      members: arr,
+    };
+    this.generateRow = this.generateRow.bind(this);
+  }
 
-    generateRow(obj, additionalClas) {
-        return (<div className={additionalClas ? additionalClas.clas + " membersRow" : "membersRow"}>
-            <div >
-                {obj.Name}
-            </div>
-            <div>
-                {obj.Email}
-            </div>
-            <div>
-                {obj.CONTACT}
-            </div>
-        </div>
-        )
-    }
+  generateRow(obj, additionalClas) {
+    return (
+      <div
+        className={
+          additionalClas ? additionalClas.clas + " membersRow" : "membersRow"
+        }
+      >
+        <div>{obj.Name}</div>
+        <div>{obj.Email}</div>
+        <div>{obj.CONTACT}</div>
+        <div>{obj.Address}</div>
+      </div>
+    );
+  }
 
-    render() {
-
-        return( <> 
-        {
-            this.generateRow({ Name: "NAME", Email: "EMAIL", "CONTACT": "CONTACT" },
-                { clas: "membersHeader" })}
+  render() {
+    return (
+      <>
+        {this.generateRow(
+          { Name: "NAME", Email: "EMAIL", CONTACT: "PHONE","Address":"ADDRESS" },
+          { clas: "membersHeader" }
+        )}
         <div className="membersTable">
-            {this.state.members.map((obj) => this.generateRow(obj))}</div></>)
-    }
+          {this.state.members.map((obj) => this.generateRow(obj))}
+        </div>
+      </>
+    );
+  }
 }
