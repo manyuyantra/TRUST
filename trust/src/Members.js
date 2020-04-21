678169f2def0a4c1fcfd5d2b2e20601766815489
sriram21088 <sriram21088@gmail.com>
2020-04-21 17:53:02 +0530

    Members grid added..

--- trust/src/Members.js
+++ trust/src/Members.js
@@ -1,6 +1,48 @@
- 
+import React from 'react'
 
 
-export default function Members(){
-    return "members";
+export default class Members extends React.Component {
+    constructor(pr) {
+        super(pr);
+        this.state = {
+            "members": [
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" },
+                { "Name": "Dummy", "Email": "DDUMMY@MAILME.COM", "CONTACT": "#922311" }
+
+
+            ]
+
+        }
+        this.generateRow = this.generateRow.bind(this);
+    }
+
+    generateRow(obj, additionalClas) {
+        return (<div className={additionalClas ? additionalClas.clas + " membersRow" : "membersRow"}>
+            <div >
+                {obj.Name}
+            </div>
+            <div>
+                {obj.Email}
+            </div>
+            <div>
+                {obj.CONTACT}
+            </div>
+        </div>
+        )
+    }
+
+    render() {
+        return <div className="membersTable">{
+            this.generateRow({ Name: "NAME", Email: "EMAIL", "CONTACT": "CONTACT" },
+                { clas: "membersHeader" })}
+            {this.state.members.map((obj) => this.generateRow(obj))}</div>
+    }
 }
